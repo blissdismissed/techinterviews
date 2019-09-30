3145b8f8e9f4eda4fdbf30967e607c5ffebb37d6
Arthur <ajautz@gmail.com>
2019-09-30 13:36:27 -0400

    ready

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+import sys
 import math
+import requests
 
+x = requests.get('https://w3schools.com/python/demopage.htm')
+
+print(x.text)
 
 if __name__ == '__main__':
     print('test')
+    num = sys.argv[1].split(",")
+    print(num)
